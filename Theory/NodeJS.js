e216e2b8a6223e39fa14ef54f6613a1b0b5bfa9b
krishna9849 <yellaprkrishna@gmail.com>
2023-03-10 13:20:18 +0530

    node js updated

---
+++ Theory/NodeJS.js
@@ -0,0 +1,15 @@
+/*
+    --------- Intro to NodeJS-------------
+
+        1. Javascript Runtime Environment
+        2. Run on V8 engine which is developed using c++ implements ECMAscript and webassembly code
+        3. scalable , high performance , open source
+        4. server side development
+
+------------ NodeJs Ecosystem and Event loop ---------------
+
+    
+
+
+
+*/
